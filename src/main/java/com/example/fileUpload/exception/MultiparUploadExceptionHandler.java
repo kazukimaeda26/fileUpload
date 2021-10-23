56ff8e192950f4a65ecdbe54feb07d18f78b250e
前田　一樹 <maedakazuki@LAB-N1537.local>
2021-10-23 15:17:12 +0900

    128KBを超えた時の処理

---
+++ src/main/java/com/example/fileUpload/exception/MultiparUploadExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.fileUpload.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestControllerAdvice
+public class MultiparUploadExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleFileUploadException(MaxUploadSizeExceededException exception, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        return "File Size limit exceeded. Please make sure the file size is well within 128KB";
+    }
+}
